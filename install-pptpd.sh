713d27221227fecf29a0aed937d8e6c01fab329b
Franklin Dong <dongxf@gmail.com>
2012-12-29 17:25:56 +0000

    modified to work with /etc/init.d/iptables

--- install-pptpd.sh
+++ install-pptpd.sh
@@ -9,62 +9,68 @@
 # =================================
 #
 # Authors: dongxf@gmail.com
-# Version: 0.1
+# Version: 0.2
 # URL: http://dongxf.sowact.com/
 
-#echo "[ Clear previous settting... ]"
-#apt-get -y remove pptpd ppp
-#iptables --flush POSTROUTING --table nat
-#iptables --flush FORWARD
-#rm -rf /etc/pptpd.conf
-#rm -rf /etc/ppp
-
 echo "[ Install packages... ]"
-#mkdir ~/src
-#cd ~/src
-#wget http://www.bradiceanu.net/files/pptpd-1.3.4-1.fc12.src.rpm
-#rpmbuild --rebuild pptpd-1.3.4-1.fc12.src.rpm
-#rpm -i ../rpmbuild/RPMS/i686/pptpd-1.3.4-1.amzn1.i386.rpm
-#apt-get update
-apt-get -y install chkconfig pptpd ppp
+apt-get -y install bcrelay pptpd
+
+backup_dir=/etc/backup.install-pptpd.`date +%Y%m%d%H%M%S`
+mkdir $backup_dir
 
 echo "[ Allocate ip to vpn clients... ]"
+cp /etc/pptpd.conf $backup_dir/pptpd.conf.before
 sed -i 's/^logwtmp/#logwtmp/g' /etc/pptpd.conf
 echo "localip 10.64.64.1" >> /etc/pptpd.conf
 echo "remoteip 10.64.64.2-100" >> /etc/pptpd.conf
+cp /etc/pptpd.conf $backup_dir/pptpd.conf.after
 
 echo "[ Setup ipv4 forwarding... ]"
+cp /etc/sysctl.conf $backup_dir/sysctrl.conf.before
 sed -i 's/^net.ipv4.ip_forward/#net.ipv4.ip_forward/g' /etc/sysctl.conf
 echo "net.ipv4.ip_forward = 1" >> /etc/sysctl.conf
+cp /etc/sysctl.conf $backup_dir/sysctrl.conf.after
 sysctl -p
 
 echo "[ Setup dns... ]"
+cp /etc/ppp/pptpd-options $backup_dir/pptpd-options.before
 echo "ms-dns 8.8.8.8" >> /etc/ppp/pptpd-options
 echo "ms-dns 8.8.4.4" >> /etc/ppp/pptpd-options
+cp /etc/ppp/pptpd-options $backup_dir/pptpd-options.after
 
 echo "[ Generating password for vpn client... ]"
+cp /etc/ppp/chap-secrets $backup_dir/chap-secrets.before
 pass=`openssl rand 8 -base64`
 if [ "$1" != "" ]
 then pass=$1
 fi
 echo "vpn pptpd ${pass} *" >> /etc/ppp/chap-secrets
-echo "dongxy pptpd yourpasswd *" >> /etc/ppp/chap-secrets
-echo "dongxf pptpd yourpasswd *" >> /etc/ppp/chap-secrets
-echo "x201i pptpd yourpasswd *" >> /etc/ppp/chap-secrets
-echo "carify pptpd yourpasswd *" >> /etc/ppp/chap-secrets
-
+cp /etc/ppp/chap-secrets $backup_dir/chap-secrets.after
 
 echo "[ Change iptables... ]"
+iptables-save > $backup_dir/iptables.dump.before
 src_ip=`ifconfig  | grep 'inet addr:'| grep -v '127.0.0.1' | cut -d: -f2 | awk 'NR==1 { print $1}'`
 iptables -t nat -A POSTROUTING -s 10.64.64.0/24 -j SNAT --to-source $src_ip
+iptables -A INPUT -p tcp -m tcp --dport 1723 -j ACCEPT
 iptables -A FORWARD -p tcp --syn -s 10.64.64.0/24 -j TCPMSS --set-mss 1356
-iptables-save > /etc/iptables_up.rules
-echo "pre-up iptables-restore < /etc/iptables_up.rules" >> /etc/network/interfaces
+iptables -A INPUT -p gre -j ACCEPT
+iptables-save > $backup_dir/iptables.dump.after
+sed -i 's/:FORWARD DROP/:FORWARD ACCEPT/g' $backup_dir/iptables.dump.after
+iptables-restore < $backup_dir/iptables.dump.after
+if [ -f /etc/default/iptables ]; then
+  cp /etc/default/iptables $backup_dir/iptables.before
+  cp $backup_dir/iptables.dump.after /etc/default/iptables
+  cp /etc/default/iptables $backup_dir/iptables.after
+else
+  cp /etc/network/interfaces $backup_dir/interfaces.before
+  echo "pre-up iptables-restore < $backup_dir/iptables_up.dump.after" >> /etc/network/interfaces
+  cp /etc/network/interfaces $backup_dir/interfaces.after
+fi
 
 echo "[ Enable and start pptpd service ... ]"
-#chkconfig iptables on
-chkconfig pptpd on
+update-rc.d pptpd enable
+service iptables restart
 service pptpd start
 
-echo -e "\nDarling, pptpd service has been installed & enabled,  default client is \033[1mvpn\033[0m, password is \033[1m${pass}\033[1m"
+echo -e "\nDarling, pptpd service has been installed & enabled,  default client is \033[42;37mvpn\033[0m, password is \033[44;31m${pass}\033[0m"
 echo -e "Please using telnet www.yourdomain.com 1723 to verify your Security Groups settings"
