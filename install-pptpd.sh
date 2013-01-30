c64570509af5e463d12a3ffbe55fedc9c434e1f6
Ubuntu <ubuntu@ip-10-160-78-94.ap-northeast-1.compute.internal>
2013-01-30 06:32:35 +0000

    modify 10.64.64 to eth0

--- install-pptpd.sh
+++ install-pptpd.sh
@@ -55,9 +55,10 @@ cp /etc/ppp/chap-secrets $backup_dir/chap-secrets.after
 echo "[ Change iptables... ]"
 iptables-save > $backup_dir/iptables.dump.before
 src_ip=`ifconfig  | grep 'inet addr:'| grep -v '127.0.0.1' | cut -d: -f2 | awk 'NR==1 { print $1}'`
-iptables -t nat -A POSTROUTING -s 10.64.64.0/24 -j SNAT --to-source $src_ip
+#iptables -t nat -A POSTROUTING -s 10.64.64.0/24 -j SNAT --to-source $src_ip
+iptables -t nat -A POSTROUTING -o eth0 -j SNAT --to-source $src_ip
 iptables -A INPUT -p tcp -m tcp --dport 1723 -j ACCEPT
-iptables -A FORWARD -p tcp --syn -s 10.64.64.0/24 -j TCPMSS --set-mss 1356
+iptables -A FORWARD -p tcp --syn -o eth0 -j TCPMSS --set-mss 1356
 iptables -A INPUT -p gre -j ACCEPT
 iptables-save > $backup_dir/iptables.dump.after
 sed -i 's/:FORWARD DROP/:FORWARD ACCEPT/g' $backup_dir/iptables.dump.after
